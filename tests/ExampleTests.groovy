f245e81e10fba40babd72df95d15fe189e6d39dd
Christopher Johannsen <chris@Christophers-MacBook-Pro.local>
2014-10-03 07:27:33 -0700

    cleaning up by removing closures project

--- tests/ExampleTests.groovy
+++ tests/ExampleTests.groovy
@@ -3,13 +3,13 @@ package tests
 /**
  * Created by chris on 9/25/14.
  */
-class ExampleTests  extends GroovyTestCase
+class ExampleTests extends GroovyTestCase
 {
     private workers
 
     void setup()
     {
-        workers = [Chris: "Footman", Seth: "Butler", Aaron: "Lord's Valet", Jason: "First Footman", Tom: "Lord", Palle: "Valet"]
+
     }
 
     void testSimpleClosure()
@@ -19,11 +19,13 @@ class ExampleTests  extends GroovyTestCase
 
     void testMapEach ()
     {
+        workers = [Chris: "Footman", Seth: "Butler", Aaron: "Lord's Valet", Jason: "First Footman", Tom: "Lord", Palle: "Valet"]
         workers.each(){ name, occupation -> print "$name is a fine $occupation at Downton Abbey" }
     }
 
     void testMapCollect ()
     {
+        workers = [Chris: "Footman", Seth: "Butler", Aaron: "Lord's Valet", Jason: "First Footman", Tom: "Lord", Palle: "Valet"]
         def actual = workers.collect() { name, occupation -> $name + ":" + $occupation }
         assertTrue(actual.size() == 6)
     }
