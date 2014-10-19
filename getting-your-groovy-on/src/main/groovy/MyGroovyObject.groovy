a4aa5e8943504fd5dedf98c6722a06c1748ccbd0
Christopher Johannsen <chrisbjohannsen@gmail.com>
2014-10-19 08:06:05 -0700

    adding tests and a simple class to test

--- getting-your-groovy-on/src/main/groovy/MyGroovyObject.groovy
+++ getting-your-groovy-on/src/main/groovy/MyGroovyObject.groovy
@@ -2,6 +2,11 @@
  * Created by chris on 10/15/14.
  */
 class MyGroovyObject {
+
+    String prop1
+    String prop2
+    String name
+
     def myOriginalMethod() {'Original Method'}
     def mySecondMethod () {'Second Method'}
     def myClosureProperty = { 'I am a closure' }
--- getting-your-groovy-on/src/test/groovy/ExamplesTests.groovy
+++ getting-your-groovy-on/src/test/groovy/ExamplesTests.groovy
@@ -4,27 +4,78 @@
 */
 class ExampleTests extends GroovyTestCase
 {
-    private workers
+    private workers = [William: "Footman", Charlie: "Butler", John: "Lord's Valet", Thomas: "First Footman", Robert: "Lord", Tom: "Chauffer"]
 
+    /* Basic Closure Tests */
     void testSimpleClosure()
     {
         10.times(){ println "Hello from iteration $it"}
     }
 
+
+    /* Collections Tests*/
     void testMapEach ()
     {
-        workers = [Chris: "Footman", Seth: "Butler", Aaron: "Lord's Valet", Jason: "First Footman", Tom: "Lord", Palle: "Valet"]
         workers.each(){ name, occupation -> println "$name is a fine $occupation at Downton Abbey" }
     }
 
     void testMapCollect ()
     {
-        workers = [Chris: "Footman", Seth: "Butler", Aaron: "Lord's Valet", Jason: "First Footman", Tom: "Lord", Palle: "Valet"]
         def actual = workers.collect() { name, occupation -> name + " is a " + occupation }
         assertTrue(actual.size() == 6)
-        assertEquals(actual[0], "Chris is a Footman")
+        assertEquals(actual[0], "William is a Footman")
     }
 
+    void testMapFind ()
+    {
+        def actual = workers.find { it.key ==  "John" }
+        assertEquals(actual.value, "Lord's Valet")
+    }
+
+    void testMapFindAll()
+    {
+        def actual = workers.findAll {it.value.contains("a")}
+        assertEquals(actual.size(), 4)
+    }
+
+    void testMapAny()
+    {
+        def actual = workers.any { it.key == "Tom" }
+        assertTrue(actual)
+    }
+
+    void testMapEvery()
+    {
+        def actual = workers.every { it.key.size() > 0 }
+        assertTrue(actual)
+    }
+
+    void testMapGroupBy() {
+        def offices = [
+                new MyGroovyObject(name: "AELN", prop1: "Bend", prop2: "Oregon"),
+                new MyGroovyObject(name: "HQ", prop1: "San Diego", prop2: "California"),
+                new MyGroovyObject(name: "Foundation", prop1: "San Diego", prop2: "California"),
+                new MyGroovyObject(name: "PLP", prop1: "San Diego", prop2: "California"),
+                new MyGroovyObject(name: "LIMS", prop1: "Milford", prop2: "Massachusetts"),
+                new MyGroovyObject(name: "LES", prop1: "Milford", prop2: "Massachusetts"),
+                new MyGroovyObject(name: "Discoverant", prop1: "Boulder", prop2: "Colorado"),
+                new MyGroovyObject(name: "MakeBelieve", prop1: "Boulder", prop2: "Wyoming"),
+        ]
+
+        def byCity = offices.groupBy { office -> office.prop1 }
+
+        byCity.each {println it}
+        assertTrue byCity["Bend"].size() == 1
+        assertTrue byCity["Boulder"].size() == 2
+        assertTrue byCity["Milford"].size() == 2
+        assertTrue byCity["San Diego"].size() == 3
+
+        def plp = offices.find{ it.name == "PLP" }
+        assertTrue(byCity["San Diego"].contains(plp))
+        assertTrue( byCity["Boulder"].find{ it.name == "Discoverant"}.prop2 == "Colorado" )
+    }
+
+    /* MetaObjectClass Tests*/
     void testMOPMethodInterception()
     {
         def myObject = new MyGroovyObject()
@@ -46,4 +97,7 @@ class ExampleTests extends GroovyTestCase
 
         assertTrue(actual.isPalindrome())
     }
+
+    /*  Mocking  */
+
 }
